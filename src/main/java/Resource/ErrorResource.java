621adf576bf2d7e213f7564a132d4a312600526d
starwook <star.wookk@gmail.com>
2022-12-16 03:51:16 +0900

    feat: Add ErrorResource.java

---
+++ src/main/java/Resource/ErrorResource.java
@@ -0,0 +1,11 @@
+package Resource;
+
+public class ErrorResource {
+    public static String ERROR_START ="[ERROR]";
+    public static String NUMBER_NOT_IN_RANGE ="범위 바깥의 숫자입니다";
+    public static String NOT_NUMBER = "숫자가 아닙니다";
+    public static String NUMBER_DUPLICATE ="숫자가 겹칩니다";
+    public static String NOT_WINNING_NUMBER ="당첨번호 입력이 잘못되었습니다";
+    public static String NUMBER_SIZE_WRONG ="숫자 갯수가 잘못되었습니다";
+    public static String PURCHASE_WRONG ="구입 금액이 잘못되었습니다";
+}
