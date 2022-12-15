803f89483b813a37713ab836b741f5210c40487b
starwook <star.wookk@gmail.com>
2022-12-15 20:19:35 +0900

    feat: Add checkNumberInRange method

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -19,6 +19,13 @@ public class Lotto {
             throw new IllegalArgumentException();
         }
     }
+    private void checkNumberInRange(List<Integer> numbers){
+        for(int i=0;i<numbers.size();i++){
+            if(numbers.get(i)<1 || numbers.get(i)>45){
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 
     // TODO: 추가 기능 구현
 }
