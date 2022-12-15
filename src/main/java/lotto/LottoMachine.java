b6636316ccba8962a09d63d33a79bd6e9d80829d
starwook <star.wookk@gmail.com>
2022-12-15 20:35:00 +0900

    feat: Add numbersInRange method

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -4,7 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LottoMachine {
-    WinningLotto winningLotto;
-    List<Lotto> lottos = new ArrayList<>();
+    private static int LOTTO_PRICE =1000;
+    private static int LOTTO_SIZE =6;
+    private static int RANGE_START =1;
+    private static int RANGE_END =45;
+    private WinningLotto winningLotto;
+    private List<Lotto> lottos = new ArrayList<>();
+    private int lottoCount;
+    private double returnRate;
+
+    public LottoMachine(int purchase,List<Integer> numberInput,int bonusNumberInput){
+        validatePurchase(purchase);
+        validateNumbersInRange(numberInput);
+    }
+
+    private void validateNumbersInRange(List<Integer> numberInput) {
+        for(int i = 0; i< numberInput.size(); i++){
+            if(numberInput.get(i)>RANGE_END || numberInput.get(i)<RANGE_START){
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+    private void validatePurchase(int purchase) {
+        if(purchase %LOTTO_PRICE!=0 && purchase ==0){
+            throw new IllegalArgumentException();
+        }
+        lottoCount = purchase /LOTTO_PRICE;
+    }
+
 
 }
