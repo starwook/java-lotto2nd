89248f61e87356658015ac2deaa2ef026dd30e85
starwook <star.wookk@gmail.com>
2022-12-15 21:11:33 +0900

    feat: Add countingMathingNumber method

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -24,4 +24,14 @@ public class Lotto {
     public List<Integer> getNumbers(){
         return numbers;
     }
+
+    public int countMatchingNumber(List<Integer> winningNumbers){
+        int matchCount =0;
+        for(int i=0;i<winningNumbers.size();i++){
+            if(numbers.contains(winningNumbers.get(i))){
+                matchCount++;
+            }
+        }
+        return matchCount;
+    }
 }
