ad3c8da05f8552d2f134ecc7ad535f66d6292f23
starwook <star.wookk@gmail.com>
2022-12-15 21:16:36 +0900

    feat: Add checkContainBonusNumber method

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -34,4 +34,10 @@ public class Lotto {
         }
         return matchCount;
     }
+    public boolean checkContainBonusNumber(int bonusNumber){
+        if (numbers.contains(bonusNumber)){
+            return true;
+        }
+        return false;
+    }
 }
