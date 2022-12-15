d09c8fa5e455bbeafb12992c68874910211743c9
starwook <star.wookk@gmail.com>
2022-12-15 22:17:44 +0900

    feat: Add makeCount.java

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -1,10 +1,24 @@
 package lotto;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class LottoResult {
+    private static int START_COUNT =0;
     Map<Rank,Integer> rankCount = new LinkedHashMap<>();
+    int totalReward;
+    double sumRate;
 
+    public LottoResult(){
+        makeRankCount();
+    }
+
+    private void makeRankCount() {
+        List<Rank> ranks = List.of(Rank.values());
+        for(int i=0;i<ranks.size();i++){
+            rankCount.put(ranks.get(i),START_COUNT);
+        }
+    }
 
 }
