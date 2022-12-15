dbec854ba63181ef817941126e1f5edab5c97460
starwook <star.wookk@gmail.com>
2022-12-15 20:22:00 +0900

    feat: Add LottoMachine.java

---
+++ src/main/java/lotto/LottoMachine.java
@@ -0,0 +1,10 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoMachine {
+    WinningLotto winningLotto;
+    List<Lotto> lottos = new ArrayList<>();
+
+}
