9fafc522373b98cc2c1fd884d89e9aed12dbbb17
starwook <star.wookk@gmail.com>
2022-12-16 03:51:27 +0900

    feat: Add validate method

--- src/main/java/lotto/WinningLotto.java
+++ src/main/java/lotto/WinningLotto.java
@@ -1,13 +1,23 @@
 package lotto;
 
+import Resource.ErrorResource;
+
 public class WinningLotto {
     private Lotto lotto;
     private Integer bonusNumber;
 
     public WinningLotto(Lotto lotto,Integer bonusNumber){
+        validateWinningLotto(lotto, bonusNumber);
         this.lotto = lotto;
         this.bonusNumber = bonusNumber;
     }
+
+    private void validateWinningLotto(Lotto lotto, Integer bonusNumber) {
+        if(lotto.getNumbers().contains(bonusNumber)){
+            throw new IllegalArgumentException(ErrorResource.ERROR_START+ErrorResource.NUMBER_DUPLICATE);
+        }
+    }
+
     public Lotto getLotto(){
         return lotto;
     }
