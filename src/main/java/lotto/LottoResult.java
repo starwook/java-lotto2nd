4a061dce61ddcd93017c46e3a81acba6b8e632d5
starwook <star.wookk@gmail.com>
2022-12-15 22:37:39 +0900

    feat: Add changeRankCount method

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -7,10 +7,12 @@ import java.util.Map;
 public class LottoResult {
     private static int START_COUNT =0;
     Map<Rank,Integer> rankCount = new LinkedHashMap<>();
-    int totalReward;
+    int totalPrice =0;
+    int purchase;
     double sumRate;
 
-    public LottoResult(){
+    public LottoResult(int purchase){
+        this.purchase = purchase;
         makeRankCount();
     }
 
@@ -19,6 +21,27 @@ public class LottoResult {
         for(int i=0;i<ranks.size();i++){
             rankCount.put(ranks.get(i),START_COUNT);
         }
+    }
+    public void changeRankCount(Rank rank){
+        rankCount.put(rank,rankCount.get(rank)+1);
+        int price = Integer.parseInt(rank.getWinningMoney().replaceAll(",",""));
+        totalPrice+=price;
+    }
+
+    public void calculateSumRate(){
+        sumRate = Math.round(totalPrice/(double)purchase*10000)/100.0;
+    }
+
+    public double getSumRate(){
+        return sumRate;
+    }
+
+    public Map<Rank,Integer> getRankCount(){
+        return rankCount;
+    }
+
+    public int getTotalPrice(){
+        return totalPrice;
     }
 
 }
